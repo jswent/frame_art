e2bc9953fcb5edd46527750b08041604574d0faf
James Swent <jake@osprey21.com>
2025-01-24 17:59:06 -0500

    Import samsungtvws, credit @NickWaterton @xchwarze

---
+++ samsungtvws/version.py
@@ -0,0 +1,9 @@
+# Store the version here so:
+# 1) we don't load dependencies by storing it in __init__.py
+# 2) we can import it in setup.py for the same reason
+# 3) we can import it into your module module
+
+# version 3.0.0 N Waterton Dec 2025 - added version number.
+# version 3.0.1 N Waterton  16th Jan 2025 added async_art_remove_mats.py example, fixed art_remove_mats.py
+
+__version__ = '3.0.1'
