dce5d077f0ee2d834c7715f7d5496f935160d3ac
James Swent <jake@osprey21.com>
2025-01-25 01:51:31 -0500

    Add some more sensors

--- const.py
+++ const.py
@@ -9,6 +9,8 @@ CONFIG_ENTRY_KEY = "hubs"
 DEFAULT_TIMEOUT = 10.0
 DEFAULT_PORT = 8002
 
+ENABLE_SENSOR = False
+
 SENSOR_TYPES = {
     "art_mode_status": {
         "name": "Art Mode Status",
@@ -19,12 +21,41 @@ SENSOR_TYPES = {
         "name": "Brightness Level",
         "unit_of_measurement": "%",
         "icon": "mdi:brightness-6",
+        "min": 0,
+        "max": 100,
     },
     "connection_status": {
         "name": "Connection Status",
         "unit_of_measurement": None,
         "icon": "mdi:wifi",
     },
+    "color_temperature": {
+        "name": "Color Temperature",
+        "unit_of_measurement": None,
+        "icon": "mdi:thermometer",
+        "min": -5,
+        "max": 5,
+    },
+    "slideshow_status": {
+        "name": "Slideshow Status",
+        "unit_of_measurement": None,
+        "icon": "mdi:image",
+    },
+    "current_image": {
+        "name": "Current Image",
+        "unit_of_measurement": None,
+        "icon": "mdi:image",
+    },
 }
 
-SUPPORTED_PLATFORMS = ["switch"]
+SERVICE_SET_BRIGHTNESS = "set_brightness"
+SERVICE_SET_COLOR_TEMPERATURE = "set_color_temperature"
+
+ATTR_BRIGHTNESS = "brightness"
+ATTR_COLOR_TEMPERATURE = "color_temperature"
+
+SUPPORTED_PLATFORMS = [
+    "switch",
+    "sensor",
+    "media_player",
+]
--- sensor.py
+++ sensor.py
@@ -2,9 +2,8 @@ import logging
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
 from . import FrameArtHub, DOMAIN
-from .const import SENSOR_TYPES
+from .const import SENSOR_TYPES, ENABLE_SENSOR
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,7 +12,6 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities
 ) -> None:
     """Set up sensors for Frame Art integration from a config entry."""
-    _LOGGER.info("Setting up Frame Art sensors for host: %s", entry.data["host"])
 
     # Retrieve the hub for this config entry
     hub: FrameArtHub = hass.data[DOMAIN].get(entry.entry_id)
@@ -21,12 +19,11 @@ async def async_setup_entry(
         _LOGGER.error("Hub not found for entry_id: %s", entry.entry_id)
         return
 
-    # Create and add sensor entities for this hub
-    sensors = [
-        FrameArtSensor(hub, sensor_type, config)
-        for sensor_type, config in SENSOR_TYPES.items()
-    ]
-    async_add_entities(sensors)
+    # Create and add sensor entities for this hub if enabled
+    if ENABLE_SENSOR:
+        _LOGGER.info("Setting up Frame Art sensors for host: %s", entry.data["host"])
+        sensors = [FrameArtSensor(hub, sensor_type, config) for sensor_type, config in SENSOR_TYPES.items()]
+        async_add_entities(sensors)
 
 
 class FrameArtSensor(SensorEntity):
@@ -41,23 +38,87 @@ class FrameArtSensor(SensorEntity):
         self._attr_unit_of_measurement = config.get("unit_of_measurement")
         self._attr_icon = config.get("icon")
         self._attr_native_value = None
+        self._attr_native_min_value = config.get("min")
+        self._attr_native_max_value = config.get("max")
+        _LOGGER.debug("Setting up sensor %s for hub %s", self._type, self._hub.name)
 
     async def async_update(self) -> None:
         """Fetch the latest state of the sensor."""
+        _LOGGER.debug("Updating sensor %s for hub %s", self._type, self._hub.name)
 
-        if self._type == "art_mode_status":
-            self._attr_native_value = await self._hub.ex(self._hub._tv.get_artmode)
-        elif self._type == "brightness_level":
-            info = await self._hub.ex(self._hub._tv.get_brightness)
-            try:
-                value = int(info.get("value", 0))
-                self._attr_native_value = value * 10
-            except (ValueError, TypeError):
-                _LOGGER.warning("Could not parse brightness value from: %s", info)
-                self._attr_native_value = 0
-        elif self._type == "connection_status":
-            self._attr_native_value = (
-                "Connected"
-                if await self._hub.ex(self._hub._tv.is_alive)
-                else "Disconnected"
-            )
+        # Check if TV is available
+        try:
+            is_alive = await self._hub.ex(self._hub._tv.is_alive)
+            self._attr_available = is_alive
+        except Exception as e:
+            _LOGGER.debug("Error checking TV availability: %s", str(e))
+            self._attr_available = False
+            return
+
+        if not self._attr_available:
+            return
+
+        try:
+            if self._type == "art_mode_status":
+                value = await self._hub.ex(self._hub._tv.get_artmode)
+                if value is None:
+                    self._attr_available = False
+                    return
+                self._attr_native_value = value
+
+            elif self._type == "brightness_level":
+                info = await self._hub.ex(self._hub._tv.get_brightness)
+                if info is None:
+                    self._attr_available = False
+                    return
+                try:
+                    value = int(info.get("value", 0))
+                    self._attr_native_value = value * 10
+                except (ValueError, TypeError):
+                    _LOGGER.warning("Could not parse brightness value from: %s", info)
+                    self._attr_native_value = 0
+
+            elif self._type == "connection_status":
+                self._attr_native_value = "Connected" if is_alive else "Disconnected"
+
+            elif self._type == "color_temperature":
+                info = await self._hub.ex(self._hub._tv.get_color_temperature)
+                if info is None:
+                    self._attr_available = False
+                    return
+                try:
+                    value = int(info.get("value", 0))
+                    self._attr_native_value = value
+                except (ValueError, TypeError):
+                    _LOGGER.warning("Could not parse color temperature value from: %s", info)
+                    self._attr_native_value = 0
+
+            elif self._type == "slideshow_status":
+                info = await self._hub.ex(self._hub._tv.get_slideshow_status)
+                if info is None:
+                    self._attr_available = False
+                    return
+                value = info.get("value")
+                if value is None:
+                    self._attr_available = False
+                    return
+                self._attr_native_value = value
+
+            elif self._type == "current_image":
+                info = await self._hub.ex(self._hub._tv.get_current)
+                if info is None:
+                    self._attr_available = False
+                    return
+                value = info.get("content_id")
+                if value is None:
+                    self._attr_available = False
+                    return
+                self._attr_native_value = value
+                    
+            else:
+                _LOGGER.warning("Unknown sensor type: %s", self._type)
+                self._attr_native_value = None
+
+        except Exception as e:
+            _LOGGER.error("Error updating %s sensor: %s", self._type, str(e))
+            self._attr_available = False
